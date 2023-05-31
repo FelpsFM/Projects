0167a978327b9e0f45386f98a75fd3f51570812e
Felps.fm <felps.rj99@gmail.com>
2023-05-31 10:55:09 -0300

    style

--- Site genérico prog/src/scripts/script.js
+++ Site genérico prog/src/scripts/script.js
@@ -1,21 +1,20 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
-// Seleciona os elementos
-const btnSubmit = document.querySelector('.btnSubmit');
-const btnSubmitDois = document.querySelector('.btnSubmitDois');
-const modalMain = document.querySelector('.modalMain');
-const modalClose = document.querySelector('.modalClose');
+ // Seleciona os elementos relevantes
+ const btnSubmit = document.querySelector('.header__btnSubmit');
+ const btnSubmitDois = document.querySelector('#joinbtn');
+ const modalMain = document.querySelector('.modalMain');
+ const modalClose = document.querySelector('.modalClose');
 
-// Função para abrir a modal
-function openModal() {
-  modalMain.showModal();
-}
+ // Função para abrir a modal
+ function openModal() {
+   modalMain.showModal();
+ }
 
-// Função para fechar a modal
-function closeModal() {
-  modalMain.close();
-}
+ // Função para fechar a modal
+ function closeModal() {
+   modalMain.close();
+ }
 
-// Adiciona os eventos de clique aos botões
-btnSubmit.addEventListener('click', openModal);
-btnSubmitDois.addEventListener('click', openModal);
-modalClose.addEventListener('click', closeModal);
+ // Adiciona os eventos de clique aos botões
+ btnSubmit.addEventListener('click', openModal);
+ btnSubmitDois.addEventListener('click', openModal);
+ modalClose.addEventListener('click', closeModal);
